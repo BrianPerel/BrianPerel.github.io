a36cbef606218f9ce76da57e18aac7353fba3455
Brian <brianperel@comcast.net>
2020-05-08 11:39:37 -0400

    'C'

--- gd_portfolio/Controller.js
+++ gd_portfolio/Controller.js
@@ -214,7 +214,7 @@ p.nominalBounds = new cjs.Rectangle(-22,-0.5,264.5,56);
 		this.btn_ad.addEventListener("click", fl_ClickToGoToAndStopAtFrame_10.bind(this));
 		
 		function fl_ClickToGoToAndStopAtFrame_10() {
-			$("#video_ins")[0].src = "IT.mp4";
+			$("#video_ins")[0].src = "IT.mkv";
 		}
 		
 		this.return_btn.addEventListener("click", a);
@@ -355,11 +355,11 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/Controller_atlas_.png?1588948904285", id:"Controller_atlas_"},
-		{src:"images/Controller_atlas_2.png?1588948904285", id:"Controller_atlas_2"},
-		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1588948904301", id:"lib/jquery-3.4.1.min.js"},
-		{src:"components/sdk/anwidget.js?1588948904301", id:"sdk/anwidget.js"},
-		{src:"components/video/src/video.js?1588948904301", id:"an.Video"}
+		{src:"images/Controller_atlas_.png?1588952116522", id:"Controller_atlas_"},
+		{src:"images/Controller_atlas_2.png?1588952116522", id:"Controller_atlas_2"},
+		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1588952116542", id:"lib/jquery-3.4.1.min.js"},
+		{src:"components/sdk/anwidget.js?1588952116542", id:"sdk/anwidget.js"},
+		{src:"components/video/src/video.js?1588952116542", id:"an.Video"}
 	],
 	preloads: []
 };

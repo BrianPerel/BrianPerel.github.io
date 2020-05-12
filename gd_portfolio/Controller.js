6cb470acde62092378bcb2be7eaf8360263484a9
Brian <brianperel@comcast.net>
2020-05-12 18:14:05 -0400

    'C'

--- gd_portfolio/Controller.js
+++ gd_portfolio/Controller.js
@@ -4,7 +4,7 @@ var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
 		{name:"Controller_atlas_", frames: [[0,0,1920,1200]]},
-		{name:"Controller_atlas_2", frames: [[1315,262,485,112],[406,747,966,101],[406,603,1128,142],[406,850,941,5],[1024,0,289,601],[1315,376,400,85],[406,407,356,68],[0,0,404,852],[1315,0,453,260],[406,0,616,405]]}
+		{name:"Controller_atlas_2", frames: [[1315,264,485,112],[406,747,966,101],[406,603,1128,142],[406,850,941,5],[1024,0,289,601],[1315,378,400,85],[406,407,356,68],[0,0,404,852],[1315,0,453,262],[406,0,616,405]]}
 ];
 
 
@@ -214,7 +214,7 @@ p.nominalBounds = new cjs.Rectangle(-22,-0.5,264.5,56);
 		this.btn_ad.addEventListener("click", fl_ClickToGoToAndStopAtFrame_10.bind(this));
 		
 		function fl_ClickToGoToAndStopAtFrame_10() {
-			$("#video_ins")[0].src = "IT2.mp4";
+			$("#video_ins")[0].src = "Lighthouse2.mp4";
 		}
 		
 		this.return_btn.addEventListener("click", a);
@@ -355,11 +355,11 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/Controller_atlas_.png?1589299949329", id:"Controller_atlas_"},
-		{src:"images/Controller_atlas_2.png?1589299949329", id:"Controller_atlas_2"},
-		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1589299949352", id:"lib/jquery-3.4.1.min.js"},
-		{src:"components/sdk/anwidget.js?1589299949352", id:"sdk/anwidget.js"},
-		{src:"components/video/src/video.js?1589299949352", id:"an.Video"}
+		{src:"images/Controller_atlas_.png?1589321614505", id:"Controller_atlas_"},
+		{src:"images/Controller_atlas_2.png?1589321614505", id:"Controller_atlas_2"},
+		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1589321614532", id:"lib/jquery-3.4.1.min.js"},
+		{src:"components/sdk/anwidget.js?1589321614532", id:"sdk/anwidget.js"},
+		{src:"components/video/src/video.js?1589321614532", id:"an.Video"}
 	],
 	preloads: []
 };
